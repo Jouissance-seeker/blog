4170094f8a83ed1bb1885c2b4856471da2385b3b
hamid shahsavani <051.sys113@gmail.com>
2025-08-26 21:07:09 +0330

    chore: new changes

--- containers/routes/global/animated-markdown.tsx
+++ containers/routes/global/animated-markdown.tsx
@@ -1,15 +1,37 @@
+'use client';
+
 import { marked } from 'marked';
 import { AnimatedSection } from './animated-section';
+import { cn } from '@/utils/cn';
+import { useEffect } from 'react';
 
-export const AnimatedMarkdown = (props: { content: string }) => {
+export const AnimatedMarkdown = (props: {
+  content: string;
+  className?: string;
+}) => {
   const tokens = marked.lexer(props.content);
+
+  // استایل‌های CSS را به صورت inline اعمال می‌کنیم
+  useEffect(() => {
+    const style = document.createElement('style');
+    style.textContent = `
+
+    `;
+    document.head.appendChild(style);
+
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, []);
+
   return (
-    <>
+    <div id="custom-prose" className={cn('w-full', props.className)}>
       {tokens.map((token, index) => {
         const htmlContent = marked.parser([token]);
         const commonProps = {
           dangerouslySetInnerHTML: { __html: htmlContent },
         };
+
         if (
           ['heading', 'paragraph', 'list', 'blockquote'].includes(token.type)
         ) {
@@ -22,6 +44,6 @@ export const AnimatedMarkdown = (props: { content: string }) => {
 
         return <div key={index} {...commonProps} />;
       })}
-    </>
+    </div>
   );
 };
