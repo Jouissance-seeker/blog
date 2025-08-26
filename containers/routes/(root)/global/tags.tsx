09a79461bd1dbf77abdebc7c77e8085abb0fe3e3
hamid shahsavani <051.sys113@gmail.com>
2025-08-26 11:39:56 +0330

    chore: new changes

--- containers/routes/(root)/global/tags.tsx
+++ containers/routes/(root)/global/tags.tsx
@@ -3,16 +3,21 @@
 import { Post } from '@/types/post';
 import { authors } from '@/constants/authors';
 import { category } from '@/constants/category';
+import { useQueryState } from 'nuqs';
+import { useRouter } from 'next/navigation';
 
 interface TagsProps {
   data: Post;
 }
 
 export const Tags = ({ data }: TagsProps) => {
+  const [, setTitle] = useQueryState('title', { defaultValue: '' });
+  const [, setAuthors] = useQueryState('authors', { defaultValue: '' });
+  const [, setCategory] = useQueryState('category', { defaultValue: '' });
+  const router = useRouter();
   if (!data.authors || !Array.isArray(data.authors)) {
     return null;
   }
-
   const categoryData = category.find((c) => c.en === data.category);
   const authorNames = data.authors
     .map((author) => {
@@ -21,11 +26,22 @@ export const Tags = ({ data }: TagsProps) => {
     })
     .join(' - ');
 
+  const handleClick = async () => {
+    await setTitle('');
+    await setAuthors(data.authors.join(','));
+    await setCategory(data.category);
+    router.push('/');
+  };
+
   return (
     <div className="flex gap-2 flex-wrap">
-      <div className="border text-sm bg-card text-card-foreground py-1.5 px-3 rounded-md transition-colors">
+      <button
+        type="button"
+        onClick={handleClick}
+        className="border text-sm bg-card text-card-foreground py-1.5 px-3 rounded-md transition-colors hover:bg-accent hover:text-accent-foreground cursor-pointer"
+      >
         {`${authorNames} / ${categoryData!.fa}`}
-      </div>
+      </button>
     </div>
   );
 };
