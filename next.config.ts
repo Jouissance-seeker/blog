61dd64b43208f5d80222d438210acc397291b77c
hamid shahsavani <051.sys113@gmail.com>
2025-08-27 10:41:04 +0330

    chore: new changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
